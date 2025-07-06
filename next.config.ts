ec2bc772e4ad7bbd276debcb3eacf14fb275634d
Daniele Bolla <bolladaniele@gmail.com>
2025-07-06 20:15:52 +0100

    adds deploy workflow

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import createMDX from "@next/mdx";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  output: "export",
+  trailingSlash: true, // GitHub Pages requires this
+  basePath: "/blog", //
 };
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,

597be0e76faf2106c117ae721598956d4366992c
Daniele Bolla <bolladaniele@gmail.com>
2025-07-08 13:36:49 +0100

    updates callout style

--- src/components/CalloutBox.tsx
+++ src/components/CalloutBox.tsx
@@ -1,6 +1,4 @@
 // src/components/CalloutBox.tsx
-import { serialize } from "next-mdx-remote/serialize";
-import { MDXRemote } from "next-mdx-remote";
 type Props = {
   type:
     | "definition"
@@ -16,36 +14,42 @@ type Props = {
 const styles = {
   definition: {
     bg: "bg-blue-50",
+    title: "text-blue-500",
     border: "border-blue-500",
     icon: "📘",
     label: "Definition",
   },
-  theorem: {
+  proposition: {
     bg: "bg-purple-50",
+    title: "text-purple-500",
     border: "border-purple-500",
     icon: "📐",
-    label: "Theorem",
+    label: "Proposition",
   },
-  proposition: {
+  theorem: {
     bg: "bg-green-50",
+    title: "text-green-500",
     border: "border-green-500",
     icon: "📌",
-    label: "Proposition",
+    label: "Theorem",
   },
   note: {
     bg: "bg-yellow-50",
+    title: "text-yellow-500",
     border: "border-yellow-500",
     icon: "📝",
     label: "Note",
   },
   example: {
     bg: "bg-orange-50",
+    title: "text-orange-500",
     border: "border-orange-500",
     icon: "📝",
     label: "Example",
   },
   remark: {
     bg: "bg-red-50",
+    title: "text-red-500",
     border: "border-red-500",
     icon: "📝",
     label: "Remark",
@@ -56,10 +60,10 @@ export default function CalloutBox({ type, title, children }: Props) {
   const style = styles[type];
 
   return (
-    <div
-      className={`border-l-4 p-4 my-6 rounded-md ${style.bg} ${style.border}`}
-    >
-      <div className="text-sm font-semibold  text-gray-700 mb-2 flex items-center gap-2">
+    <div className={`border-b-4 p-4 my-6  ${style.bg} ${style.border}`}>
+      <div
+        className={`text-sm font-bold  ${style.title} mb-2 flex items-center gap-2`}
+      >
         {/* <span>{style.icon}</span>  */} {style.label} {title}
       </div>
       <div className="text-gray-800 text-base">{children}</div>

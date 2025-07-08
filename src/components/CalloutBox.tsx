a9e6ac946965da41696c4a76edb2fc82013df77c
Daniele Bolla <bolladaniele@gmail.com>
2025-07-08 13:49:59 +0100

    updates callout styles

--- src/components/CalloutBox.tsx
+++ src/components/CalloutBox.tsx
@@ -16,6 +16,7 @@ const styles = {
     bg: "bg-blue-50",
     title: "text-blue-500",
     border: "border-blue-500",
+    header: "bg-blue-500",
     icon: "📘",
     label: "Definition",
   },
@@ -23,6 +24,7 @@ const styles = {
     bg: "bg-purple-50",
     title: "text-purple-500",
     border: "border-purple-500",
+    header: "bg-purple-500",
     icon: "📐",
     label: "Proposition",
   },
@@ -30,6 +32,7 @@ const styles = {
     bg: "bg-green-50",
     title: "text-green-500",
     border: "border-green-500",
+    header: "bg-green-500",
     icon: "📌",
     label: "Theorem",
   },
@@ -37,6 +40,7 @@ const styles = {
     bg: "bg-yellow-50",
     title: "text-yellow-500",
     border: "border-yellow-500",
+    header: "bg-yellow-500",
     icon: "📝",
     label: "Note",
   },
@@ -44,6 +48,7 @@ const styles = {
     bg: "bg-orange-50",
     title: "text-orange-500",
     border: "border-orange-500",
+    header: "bg-orange-500",
     icon: "📝",
     label: "Example",
   },
@@ -51,6 +56,7 @@ const styles = {
     bg: "bg-red-50",
     title: "text-red-500",
     border: "border-red-500",
+    header: "bg-red-500",
     icon: "📝",
     label: "Remark",
   },
@@ -60,13 +66,20 @@ export default function CalloutBox({ type, title, children }: Props) {
   const style = styles[type];
 
   return (
-    <div className={`border-b-4 p-4 my-6  ${style.bg} ${style.border}`}>
+    <div className={`  my-6  ${style.bg} ${style.border}`}>
       <div
-        className={`text-sm font-bold  ${style.title} mb-2 flex items-center gap-2`}
+        className={`text-sm font-bold text-white p-4 ${style.header} mb-2 flex items-center gap-2`}
       >
-        {/* <span>{style.icon}</span>  */} {style.label} {title}
+        {/* <span>{style.icon}</span>  */}
+        {title ? (
+          <span>
+            {style.label}: {title}
+          </span>
+        ) : (
+          style.label
+        )}
       </div>
-      <div className="text-gray-800 text-base">{children}</div>
+      <div className="text-gray-800 text-base p-4">{children}</div>
     </div>
   );
 }
